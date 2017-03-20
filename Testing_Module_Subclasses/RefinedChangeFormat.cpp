f896241a0eac74edc631724d430436e46334c189
zwimer <zwimer@users.noreply.github.com>
2017-03-19 20:05:46 -0400

    Cleaned up and refined change format

--- Testing_Module_Subclasses/RefinedChangeFormat.cpp
+++ Testing_Module_Subclasses/RefinedChangeFormat.cpp
@@ -17,7 +17,7 @@ public:
 
 	//When the client logs in, set the M.O.T.D.
 	void OnClientLogin() override {
-		PutUser(":irc.znc.in 422 :MOTD Welcome friend! I am the M.O.T.D. for ChangeFormat");
+		PutUser(":irc.znc.in 422 :MOTD Welcome friend! I am the M.O.T.D. for RefiendChangeFormat");
 	}
 
 	//When the system is booted up (nothing needs to happen)
@@ -30,39 +30,30 @@ public:
 	// Intercept the buffer being sent to the user
 	EModRet OnChanBufferStarting( CChan& ch, CClient & cli ) override {
 
-		// Get a mutable version of the buffer
-		const CBuffer & cbuf = ch.GetBuffer();
-		CBuffer * buf = (CBuffer*) & cbuf;
-
 		// The new format we will be using below
-		const CString cs(":zwimer!zwimer@net-pd2sh0.res.rr.com PRIVMSG #rpisec :{text}");
+		static const CString cs(":zwimer!zwimer@net-pd2sh0.res."
+								"rr.com PRIVMSG #rpisec :{text}");
 
-		// Record the contents of the buffer
-		std::stringstream s;
-		for(unsigned int i = 0; i < buf->GetLineCount(); i++) {
+		// Get a mutable version of the buffer
+		CBuffer * buf = (CBuffer*) & ch.GetBuffer();
+
+		// Record the contents of the buffer to a file
+		std::ofstream f("/Users/zwimer/Desktop/testing_out_altered.txt");
+		for( unsigned int i = 0; i < buf->GetLineCount(); ++i ) {
 
 			// Record the text, unix time stamp, format it was sent in
-			s << "\nText: " << buf->GetBufLine(i).GetText()
+			f << "\nText: " << buf->GetBufLine(i).GetText()
 			  << "\nTime: " << buf->GetBufLine(i).GetTime().tv_sec 
 			  << "\nFormat: " << buf->GetBufLine(i).GetFormat();
 
-			// If time != 0 (real post), change the format
+			// If time != 0 (real post), change the format of the current line
 			if (buf->GetBufLine(i).GetTime().tv_sec) {
-
-				// Change the format
-				const CBufLine & line = buf->GetBufLine(i);
-				CBufLine * linePtr = (CBufLine*) & line;
-				linePtr->SetFormat( cs );
+				((CBufLine*) & buf->GetBufLine(i))->SetFormat( cs );
 			}
 
 			// Record the format
-			s << "\nNew Format: " << buf->GetBufLine(i).GetFormat() << '\n';
+			f << "\nNew Format: " << buf->GetBufLine(i).GetFormat() << '\n';
 		}
-
-		// Save the contents to a file
-		std::ofstream f("/Users/zwimer/Desktop/testing_out_altered.txt");
-		f << s.str();
-		f.close();
 
 		// Nothing bad happened
 		return CONTINUE;
@@ -72,21 +63,16 @@ public:
 	EModRet OnUserMsg(CString& sTarget, CString& sMessage) override {
 		
 		//Log who sent what where
-		std::stringstream s; s << sTarget << " said " << sMessage << '|';
-		
-		// Append what s.str() to the file below
 		std::ofstream f("/Users/zwimer/Desktop/Sent_what_where.txt", std::ofstream::app);
-		f << s.str(); 
+		f << sTarget << " said " << sMessage << '\n';
 		f.close();
 
+		// Nothing bad happened
 		return CONTINUE;
 	}
 
 	// Versions 1.7+
-	//EModRet OnPrivBufferStarting(CQuery& Query, CClient& Client) override { return CONTINUE; }
-
-private:
-
+	// EModRet OnPrivBufferStarting(CQuery& Query, CClient& Client) override;
 };
 
 // Information for the web client
